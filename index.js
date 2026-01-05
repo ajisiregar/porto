a8fa0077c708bc6eed27b3d31df2d26cafd498fc
ajisiregar <ahmadizulkarnain@ymail.com>
2026-01-05 15:37:19 +0700

    update

---
+++ index.js
@@ -0,0 +1,12 @@
+const imgElement = document.getElementsByClassName('img_button');
+const btnElement = document.getElementsByClassName('toggleImage');
+
+isTrue = !isTrue;
+
+if (isTrue){
+    imgElement.src = 'Minus icon.svg'
+    btnElement.classList.add
+}
+btn.addEventListener('click', () => {
+    btn.classList.toggle('gridWP1_0')
+})
